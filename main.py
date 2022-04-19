c15db4101e18c194698f094a6c6544464c495e1a
tj-schultz <t.schultz@ufl.edu>
2022-04-19 15:31:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class OnlineRecognition(tk.Frame):
         self.canvas.bind("<ButtonRelease-1>", self.update_path)
 
         ## pack canvas
-        self.canvas.grid(padx=100)
+        #self.canvas.grid(padx=100)
         self.canvas.pack(side="top", fill="both", expand=False)
 
         #GUI -- Clear Canvas display
@@ -186,6 +186,9 @@ class OnlineRecognition(tk.Frame):
 
 
 class DataCollection(tk.Frame):
+    sample_types = ["T", "N", "D", "P", "X", "H", "I", "exclamation", "line",\
+                    "five-point star", "null", "arrowhead", "pitchfork", "six-point star",\
+                    "asterisk", "half-note"]
     def __init__(self, parent, *args, **kwargs):
 
         tk.Frame.__init__(self, parent, *args, **kwargs)
@@ -214,7 +217,7 @@ class DataCollection(tk.Frame):
         self.canvas.bind("<ButtonRelease-1>", self.update_path)
 
         ## pack canvas
-        self.canvas.grid(padx=100)
+        #self.canvas.grid(padx=100)
         self.canvas.pack(side="top", fill="both", expand=False)
 
         #GUI -- Clear Canvas display
@@ -223,7 +226,27 @@ class DataCollection(tk.Frame):
         self.clear_frame.pack(side="top")
         self.clear_button.pack(side="left")
 
+        ## GUI -- Prompt display
+        self.prompt_frame = tk.Frame(root)
+        self.subject_label = tk.Label(self.prompt_frame, text="SubjectID:")
+        self.drawing_label = tk.Label(self.prompt_frame, text="Drawing:")
+        self.subject_entry = tk.Entry(self.prompt_frame, width=16, text="NULL")
+        self.samplenum_label = tk.Label(self.prompt_frame, text="Sample#:")
+        self.drawing_entry = tk.Entry(self.prompt_frame, width=24)
+        self.samplenum_entry = tk.Entry(self.prompt_frame, width=3)
+        self.prompt_frame.pack(side="top")
+        self.drawing_label.pack(side="top")
+        self.samplenum_label.pack(side="left")
+        self.drawing_entry.pack(side="top")
 
+        self.samplenum_entry.pack(side="left")
+        self.subject_entry.pack(side="bottom")
+        self.subject_label.pack(side="bottom")
+
+        self.subject_entry.insert(0, "NULL")
+        self.drawing_entry.insert(0, self.sample_types[0])
+        self.samplenum_entry.insert(0, 1)
+        """
         ## GUI -- Recognizer display
         self.recog_frame = tk.Frame(root)
         self.match_label = tk.Label(self.recog_frame, text="Best Match:")
@@ -233,16 +256,13 @@ class DataCollection(tk.Frame):
         self.match_label.pack(side="left")
         self.match_entry.pack(side="left")
         self.recog_button.pack(side="bottom")
+        """
 
-
-        ## GUI -- Path length display
+        ## GUI -- Path length and submission display
         self.length_frame = tk.Frame(root)
-        self.score_label = tk.Label(self.length_frame, text="Score:")
-        self.score_entry = tk.Entry(self.length_frame, width=8)
         self.length_label = tk.Label(self.length_frame, text="Path Length:")
         self.length_entry = tk.Entry(self.length_frame, width=8)
-        self.score_label.pack(side="left")
-        self.score_entry.pack(side="left")
+        self.submit_button = tk.Button(self.length_frame, width=16, text="Log Sample", command=self.submit)
         self.length_frame.pack(side="top")
         self.length_label.pack(side="left")
         self.length_entry.pack(side="left")
@@ -291,17 +311,40 @@ class DataCollection(tk.Frame):
         info_label.pack(side="top", fill="both", expand=False)
         #github_label.pack(side="top", fill="both", expand=False)
 
+    ## submits path to file
     def submit(self):
-        ## calculate results and update results entries
-        npath = self.pathcanvas.npath
-        nrecognizer = multi_rec(useBoundedRotationInvariance=True, useProtractor=True)
-        print([s.parsed_path for s in npath.strokes])
-        results = nrecognizer.recognize([s.parsed_path for s in npath.strokes])
-        # if len(results) > 0:
-        self.score_entry.delete(0, tk.END)
-        self.score_entry.insert(0, round(results.score, 2))
-        self.match_entry.delete(0, tk.END)
-        self.match_entry.insert(0, results.name)
+        ## get current loop information
+        s_index = 0
+        s_id = self.subject_entry.get()
+        g_id = int(self.samplenum_entry.get())
+        name = self.drawing_entry.get()
+        s_index = self.sample_types.index(name)
+        name += str(g_id).zfill(2)
+
+        ## write out
+        to_xml(path=self.pathcanvas.npath, name=name, s_id=s_id, g_id=g_id)
+
+        ## increment loop
+        self.samplenum_entry.delete(0, tk.END)
+        if g_id == 10:
+            self.samplenum_entry.insert(0, 1)
+            self.drawing_entry.delete(0, tk.END)
+            if s_index < (len(self.sample_types) - 1):
+                s_index = s_index + 1
+            else:
+                s_index = 0
+                self.subject_entry.delete(0, tk.END)
+                self.subject_entry.insert(0, "NULL")
+            self.drawing_entry.insert(0, self.sample_types[s_index])
+        else:
+            self.samplenum_entry.insert(0, g_id + 1)
+
+        ## updates previous path length display
+        length = float(self.length_entry.get())
+        self.length_entry.delete(0, tk.END)
+        self.length_entry.insert(0, round(self.R_old.path_length(self.pathcanvas.path), 2) + length)
+        self.length_entry.update()
+
         self.pathcanvas.clear()
         self.length_entry.delete(0, tk.END)
         self.length_entry.insert(0, 0.0)
@@ -317,37 +360,7 @@ class DataCollection(tk.Frame):
         ## stops pen
         self.pathcanvas.pen(event)
 
-        ## get current loop information
-        s_index = 0
-        s_id = self.subject_entry.get()
-        g_id = int(self.samplenum_entry.get())
-        name = self.drawing_entry.get()
-        s_index = self.sample_types.index(name)
-        name += str(g_id).zfill(2)
 
-        ## write out
-        to_xml(path=self.pathcanvas.path, name=name, s_id=s_id, g_id=g_id)
-
-        ## increment loop
-        self.samplenum_entry.delete(0, tk.END)
-        if g_id == 10:
-            self.samplenum_entry.insert(0, 1)
-            self.drawing_entry.delete(0, tk.END)
-            if s_index < (len(self.sample_types)-1):
-                s_index = s_index + 1
-            else:
-                s_index = 0
-                self.subject_entry.delete(0, tk.END)
-                self.subject_entry.insert(0, "NULL")
-            self.drawing_entry.insert(0, self.sample_types[s_index])
-        else:
-            self.samplenum_entry.insert(0, g_id + 1)
-
-        ## updates previous path length display
-        length = float(self.length_entry.get())
-        self.length_entry.delete(0, tk.END)
-        self.length_entry.insert(0, round(self.R_old.path_length(self.pathcanvas.path), 2) + length)
-        self.length_entry.update()
 
         ## resample path
         self.pathcanvas.resampled = self.R_old.resample(self.pathcanvas.path, dollar.Dollar.prefs["n_points"])
@@ -431,12 +444,14 @@ class SelectionWindow(tk.Frame):
         self.online_button.pack(side="bottom")
         self.data_collection_button.pack(side="bottom")
 
-
     def online(self):
-        OnlineRecognition(root).pack(side="top", fill="both", expand=True)
+        window = tk.Toplevel()
+        OnlineRecognition(window).pack(side="top", fill="both", expand=True)
+
 
     def data_collection(self):
-        DataCollection(root).pack(side="top", fill="both", expand=True)
+        window = tk.Toplevel()
+        DataCollection(window).pack(side="top", fill="both", expand=True)
 
 
 if __name__ == "__main__":
@@ -453,7 +468,7 @@ if __name__ == "__main__":
     ## define window properties
     root.title("dollargeneral-recognizer")
     root.minsize(500, 700)
-    #root.maxsize(500, 700)
+    root.maxsize(500, 700)
 
     ## organize root geometry as window
     SelectionWindow(root).pack(side="top", fill="both", expand=True)
